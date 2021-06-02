9e906b4efc53467b61425c8ce6d060b8189f6722
Eddy-hub19 <sekaneduard@gmail.com>
2021-06-02 17:21:00 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,32 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const less = require("gulp-less");
+const autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('server', function() {
-
+gulp.task("server", function() {
     browserSync({
         server: {
-            baseDir: "src"
-        }
+            baseDir: "src",
+        },
     });
 
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("*.html").on("change", browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task("style", function() {
     return gulp.src("less/style.less");
-
 });
 
-gulp.task('less', function() {
-    return gulp.src('./less/style.less')
+gulp.task("less", function() {
+    return gulp
+        .src("./less/style.less")
         .pipe(autoprefixer())
         .pipe(less())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest("src/main.css"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch("less/style.less", gulp.parallel('less'));
+gulp.task("watch", function() {
+    gulp.watch("less/style.less", gulp.parallel("less"));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'less'));
+gulp.task("default", gulp.parallel("watch", "server", "less"));
