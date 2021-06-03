c9cb3e7002f4e6df9256f4ff46b0cdf4465d50ff
Eddy-hub19 <sekaneduard@gmail.com>
2021-06-03 23:12:16 +0300

    update

--- Food-Store/gulpfile.js
+++ Food-Store/gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
 const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 
 gulp.task("less", function() {
     return gulp
         .src("./style/less/style.less")
         .pipe(less())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest("./style/"));
+});
+
+gulp.task("scss", function() {
+    return gulp
+        .src("./style/scss/style.scss")
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(concat('style.css'))
         .pipe(gulp.dest("./style/"));
 });
