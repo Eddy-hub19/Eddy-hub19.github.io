91d21077b32c159367c779f0db90802c5f20d89f
Eddy-hub19 <sekaneduard@gmail.com>
2021-06-15 17:05:39 +0300

    rename

---
+++ Folder-for-Elogic/Food-Store/carousel.js
@@ -0,0 +1,19 @@
+offset = 0; // смещение от левого края
+
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".slider-next").addEventListener("click", function() {
+    offset = offset + 230;
+    if (offset > 1200) {
+        offset = 0
+    }
+    sliderLine.style.left = offset + "px";
+});
+
+document.querySelector(".slider-prev").addEventListener("click", function() {
+    offset = offset - 230;
+    if (offset > 1200) {
+        offset = 0
+    }
+    sliderLine.style.left = -offset + "px";
+});
