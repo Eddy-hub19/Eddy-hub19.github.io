17e317d42dd86fe8c3c0e7cb0cfa936f22c3cb16
Eddy-hub19 <sekaneduard@gmail.com>
2021-06-17 15:05:28 +0300

    add carousel

--- Folder-for-Elogic/Food-Store/carousel.js
+++ Folder-for-Elogic/Food-Store/carousel.js
@@ -1,41 +1,41 @@
-    $(document).ready(function() {
+$(document).ready(function() {
+    $(".slider").slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        accessibility: true,
+        mobileFirst: true,
+        infinite: true,
+    });
+});
 
-        $('.slider').slick();
+// for carousel JS
 
-        $('.autoplay').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-        });
-    })
+// offset = 0; // смещение от левого края
 
+// const sliderLine = document.querySelector(".slider-line");
 
+// document.querySelector(".slider-next").addEventListener("click", function() {
+//     offset = offset + 230;
+//     if (offset > 1000) {
+//         offset = 0;
+//     }
+//     sliderLine.style.left = offset + "px";
+// });
 
-    // offset = 0; // смещение от левого края
+// document.querySelector(".slider-prev").addEventListener("click", function() {
+//     offset = offset + 230;
+//     if (offset < 0) {
+//         offset = 230;
+//     } else {
+//         offset = 0;
+//     }
+//     sliderLine.style.left = -offset + "px";
+// });
 
-    // const sliderLine = document.querySelector(".slider-line");
+// const test = document.querySelector(".warning");
 
-    // document.querySelector(".slider-next").addEventListener("click", function() {
-    //     offset = offset + 230;
-    //     if (offset > 1000) {
-    //         offset = 0;
-    //     }
-    //     sliderLine.style.left = offset + "px";
-    // });
-
-    // document.querySelector(".slider-prev").addEventListener("click", function() {
-    //     offset = offset + 230;
-    //     if (offset < 0) {
-    //         offset = 230;
-    //     } else {
-    //         offset = 0;
-    //     }
-    //     sliderLine.style.left = -offset + "px";
-    // });
-
-    // const test = document.querySelector(".warning");
-
-    // document.querySelector(".close-btn").addEventListener("click", function() {
-    //     test.style.display = "none";
-    // });
+// document.querySelector(".close-btn").addEventListener("click", function() {
+//     test.style.display = "none";
+// });
