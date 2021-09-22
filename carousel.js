2094307d1da034f4c2618b03f4af370d8090dbe2
Eddy-hub19 <https://eddy-hub19.github.io/>
2021-09-22 17:01:19 +0300

    commit typeWritter

--- carousel.js
+++ carousel.js
@@ -1,13 +1,13 @@
-$('.carousel').slick({
-    dots: false,
-    infinite: true,
-    speed: 200,
-    fade: true,
-    cssEase: 'linear'
-  });
+$(".carousel").slick({
+  dots: false,
+  infinite: true,
+  speed: 200,
+  fade: true,
+  cssEase: "linear",
+});
 
-  const typeWriter = document.getElementById('typewriter-text');
-const text = 'TO BE CONTINUED';
+// const typeWriter = document.getElementById("typewriter-text");
+// const text = "TO BE CONTINUED";
 
-typeWriter.innerHTML = text;
-typeWriter.style.setProperty('--characters', text.length);
+// typeWriter.innerHTML = text;
+// typeWriter.style.setProperty("--characters", text.length);
