480547e5dc35290dfadbcf76c87fce7a6464d13e
Eddy-hub19 <https://eddy-hub19.github.io/>
2021-09-22 16:30:44 +0300

    changes path and add section main, add paralax and my projects

--- script.js
+++
@@ -1,5 +0,0 @@
-const typeWriter = document.getElementById('typewriter-text');
-const text = 'TO BE CONTINUED';
-
-typeWriter.innerHTML = text;
-typeWriter.style.setProperty('--characters', text.length);
