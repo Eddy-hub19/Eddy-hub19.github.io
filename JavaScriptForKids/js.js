e97b46bf128ae8b09f49694ce215d8538ee2a8f1
Eddy-hub19 <https://eddy-hub19.github.io/>
2021-11-20 17:52:24 +0200

    Cleanings

--- JavaScriptForKids/js.js
+++ JavaScriptForKids/js.js
@@ -138,32 +138,4 @@
 
 // codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
 
-let codeWord1 = "янТарь"; 
-let codeWord2 = "огурцы"; 
-let codeWord3 = "маленькие"; 
-let codeWord4 = "неофил"; 
-let codeWord5 = "рай"; 
-let codeWord6 = "виноград"; 
-let codeWord7 = "што пор"; 
-let codeWord8 = "сильный"; 
-let codeWord9 = '❤'
 
-console.log(
-    codeWord1[2] + //т
-    codeWord2[5] + //ы
-    codeWord7[3] + //
-    codeWord3[0] + // м
-    codeWord3[4] + // н
-    codeWord4[1] + // е
-    codeWord7[3] + //
-    codeWord4[0] + // н
-    codeWord5[0] + // р
-    codeWord5[1] + // а
-    codeWord6[0] + // в
-    codeWord6[1] + // и
-    codeWord7[0] + // ш
-    codeWord8[0] + // с
-    codeWord1[0] + // я
-    codeWord7[3] + //
-    codeWord9
-); 
