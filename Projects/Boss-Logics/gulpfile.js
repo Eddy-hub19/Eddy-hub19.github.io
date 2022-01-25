46c436f1267196184c08c1517bce498e5a3e1be7
Eddy20 <sekaneduard@gmail.com>
2022-01-25 18:54:02 +0200

    add gulpfile and compille files

--- Projects/Boss-Logics/gulpfile.js
+++ Projects/Boss-Logics/gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require("gulp");
 const concat = require('gulp-concat');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+
+
 
 gulp.task("sass", function() {
     return gulp
         .src("./styles/sass/style.sass")
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(concat('style.css'))
+        .pipe(concat('main.css'))
         .pipe(gulp.dest("./styles/test"));
 });
