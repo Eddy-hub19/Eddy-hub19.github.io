7b19689be4757245206458fb5339e84f0b2a9d03
Eddy20 <sekaneduard@gmail.com>
2022-02-07 15:55:07 +0200

    fix bugs with carousel

--- Projects/Food-Store/carousel.js
+++ Projects/Food-Store/carousel.js
@@ -1,31 +1,55 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $(".slider").slick({
-      slidesToShow: 4,
-      dots: true,
-      slidesToScroll: 1,
-      autoplay: true,
-      infinite: true,
-      speed: 2000,
-      accessibility: true,
-      centerMode: false,
-      useCSS: true,
-      arrows: true,
+        slidesToShow: 4,
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        speed: 2000,
+        accessibility: true,
+        centerMode: false,
+        useCSS: true,
+        arrows: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
-  });
-  
-  
-  // Show Hamburger menu
-  const hamburger = document.querySelector('.hamburger')
-  const menu = document.querySelector('.menu')
-  hamburger.addEventListener('click', function ()  {
+});
+
+
+// Show Hamburger menu
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
+hamburger.addEventListener('click', function() {
     menu.classList.toggle('show')
-  })
+})
 
 // jQuery style
 
-  // Close warning class when user click on .close-btn
-  const warning = document.querySelector(".warning");
-  
-  document.querySelector(".close-btn").addEventListener("click", function () {
+// Close warning class when user click on .close-btn
+const warning = document.querySelector(".warning");
+
+document.querySelector(".close-btn").addEventListener("click", function() {
     warning.style.display = "none";
-  });
+});
