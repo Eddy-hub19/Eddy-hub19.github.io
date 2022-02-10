0d6f48abd0e482751a48fdb5e395b96c76f59d46
Eddy20 <sekaneduard@gmail.com>
2022-02-10 22:14:49 +0200

    changes

--- Projects/Boss-Logics/gulpfile.js
+++ Projects/Boss-Logics/gulpfile.js
@@ -3,7 +3,6 @@ const concat = require('gulp-concat');
 const sass = require('gulp-sass')(require('sass'));
 
 
-
 gulp.task("sass", function() {
     return gulp
         .src("./styles/sass/style.sass")
