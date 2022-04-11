390dab4c6b69341359989f15469a5bfd4d5df6fe
Eddy20 <sekaneduard@gmail.com>
2022-04-11 16:37:34 +0300

    new changes

--- Projects/Food-Store/Full-Page-Tabs/scripts.js
+++ Projects/Food-Store/Full-Page-Tabs/scripts.js
@@ -16,11 +16,11 @@ function openPage(pageName, element, color) {
 
     // Show the specific tab content
 
-    // document.getElementById(pageName).style.display = "block";
+    document.getElementById(pageName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
     element.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
-// document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
