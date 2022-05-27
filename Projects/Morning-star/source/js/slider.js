d1fe067317ca3d452b47cac3f297886322b01607
Eddy20 <sekaneduard@gmail.com>
2022-05-27 19:33:34 +0300

    add new section and ne things

---
+++ Projects/Morning-star/source/js/slider.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 5000,
+    },
+    speed: 400,
+    loop: true,
+    allowTouchMove: true,
+    clickable: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
