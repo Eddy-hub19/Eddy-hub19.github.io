ec5275b9193a342285d54c7b0bf11bdc81f51da2
Eddy20 <sekaneduard@gmail.com>
2022-06-17 22:38:47 +0300

    old state

--- Projects/Boss-Logics/gulpfile.js
+++ Projects/Boss-Logics/gulpfile.js
@@ -1,8 +1,18 @@
+// ! IMPORTANT
+
+// npm i gulp-sass
+// npm i gulp-scss
+
+// call command to compile your code
+// gulp less
+// gulp sass
+// gulp scss
+
 const gulp = require("gulp");
 const concat = require('gulp-concat');
 const sass = require('gulp-sass')(require('sass'));
 
-
+// compile file sass to css in folder > style
 gulp.task("sass", function() {
     return gulp
         .src("./source/styles/sass/style.sass")
@@ -10,3 +20,12 @@ gulp.task("sass", function() {
         .pipe(concat('./style.css'))
         .pipe(gulp.dest("./source/styles/test"));
 });
+
+// compile file scss to css in folder > style
+// gulp.task("scss", function() {
+//     return gulp
+//         .src("./source/styles/scss/mobile.scss")
+//         .pipe(sass({ outputStyle: 'compressed' }))
+//         .pipe(concat('style.css'))
+//         .pipe(gulp.dest("./source/styles/mobile"));
+// });
