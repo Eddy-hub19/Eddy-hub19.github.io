b8a4ff7e30e5985dccaaac63e1935d9abaf91e90
Eddy20 <sekaneduard@gmail.com>
2022-06-21 20:13:33 +0300

    add new gulpfile.js

--- Projects/Boss-Logics/gulpfile.js
+++ Projects/Boss-Logics/gulpfile.js
@@ -1,29 +1,37 @@
-// Основной модуль
-import gulp from "gulp";
-// Ипорт путей
-import { path } from "./gulp/config/path.js";
+// ! IMPORTANT
 
-// Передаем значения в глобльаную переменную
-global.app = {
-    path: path,
-    path: gulp
-}
+// npm i gulp-sass
+// npm i gulp-scss
 
-// Импорт задач
-import { copy } from "./gulp/tasks/copy.js";
+// call command to compile your code
+// gulp less
+// gulp sass
+// gulp scss
 
-// Выполнение сценария по умолчанию
-gulp.task('default', copy)
+const gulp = require("gulp")
+// const less = require("gulp-less");
+const concat = require("gulp-concat")
+const sass = require("gulp-sass")(require("sass"))
 
-// const gulp = require("gulp");
-// const concat = require('gulp-concat');
-// const sass = require('gulp-sass')(require('sass'));
+// compile file scss to css in folder > style
+gulp.task("scss", function () {
+    return gulp
+        .src("./src/styles/scss/media.scss")
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(concat("media.css"))
+        .pipe(gulp.dest("./src/styles/css"))
+})
 
-// // compile file sass to css in folder > style
-// gulp.task("sass", function() {
-//     return gulp
-//         .source("./source/styles/sass/style.sass")
-//         .pipe(sass({ outputStyle: 'compressed' }))
-//         .pipe(concat('./style.css'))
-//         .pipe(gulp.dest("./source/styles/test"));
-// });
+// compile file sass to css in folder > style
+gulp.task("sass", function () {
+    return gulp
+        .src("./src/styles/sass/style.sass")
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(concat("./style.css"))
+        .pipe(gulp.dest("./src/styles/css"))
+})
+
+gulp.task('watch', function(){
+    gulp.watch('./src/styles/sass/**/*.sass', gulp.series('sass'));
+    // gulp.watch('./src/styles/scss/**/*.scss', gulp.series('scss'));
+});
