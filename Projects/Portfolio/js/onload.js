0e22d91ac52634c14bd43c75aef5d96c50f1770b
EduardSekan <sekaneduard@gmail.com>
2022-12-17 23:12:03 +0200

    add preload

---
+++ Projects/Portfolio/js/onload.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setInterval(function () {
+        const elSpinenr = document.querySelector(".preloader")
+        elSpinenr.classList.add("hide")
+    }, 3000)
+}
