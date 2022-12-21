a5cde0039e616809e6280f42dcf5ba95d28e9f99
EduardSekan <sekaneduard@gmail.com>
2022-12-21 21:36:41 +0200

    changes

--- Projects/Portfolio/js/onload.js
+++ Projects/Portfolio/js/onload.js
@@ -2,5 +2,5 @@ window.onload = function () {
     setInterval(function () {
         const elSpinenr = document.querySelector(".preloader")
         elSpinenr.classList.add("hide")
-    }, 1000)
+    }, 340)
 }
