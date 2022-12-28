c61c51c3791df1f28a15be1eb541b383a66d2ab6
EduardSekan <sekaneduard@gmail.com>
2022-12-28 18:19:11 +0200

    changes

--- Projects/Food-Store/Full-Page-Tabs/scripts.js
+++ Projects/Food-Store/Full-Page-Tabs/scripts.js
@@ -1,31 +1,37 @@
-// function openPage(pageName, element, color) {
-//     // Hide all elements with class="tabcontent" by default */
-//     let i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = 'none'
-//     }
+function openPage(pageName, element, color) {
+    // Hide all elements with class="tabcontent" by default */
+    let i, tabcontent, tablinks
+    tabcontent = document.getElementsByClassName("tabcontent")
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none"
+    }
 
-//     // Remove the background color of all tablinks/buttons
-//     tablinks = document.getElementsByClassName("tablink");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].style.backgroundColor = "";
-//     }
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink")
+    for (o = 0; o < tablinks.length; o++) {
+        tablinks[o].style.backgroundColor = ""
+    }
 
-//     // Show the specific tab content
+    // Show the specific tab content
 
-//     document.getElementById(pageName).style.display = "block";
+    document.getElementById(pageName).style.display = "block"
 
-//     // Add the specific color to the button used to open the tab content
-//     element.style.backgroundColor = color;
+    // Add the specific color to the button used to open the tab content
+    element.style.backgroundColor = color
 
-//     // Hide elements
+    // Hide elements
 
-//     // document.addEventListener('click', function(event) {
-//     //     let e = document.getElementById("test");
-//     //     if (!e.contains(event.target)) e.style.display = "none"
-//     // });
-// }
+    document.addEventListener("click", function (event) {
+        let tablink = document.querySelector(".tablink")
+        let tab = document.querySelectorAll(".tabcontent")
 
-// // Get the element with id="defaultOpen" and click on it
-// // document.getElementById("defaultOpen").click();
+        if (!tablink.contains(event.target)) {
+            tab.style.display = "block"
+        } else {
+            tab.style.display = "none"
+        }
+    })
+}
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
--- Projects/Portfolio/js/onload.js
+++ Projects/Portfolio/js/onload.js
@@ -2,5 +2,5 @@ window.onload = function () {
     setInterval(function () {
         const elSpinenr = document.querySelector(".preloader")
         elSpinenr.classList.add("hide")
-    }, 340)
+    }, 1000)
 }
