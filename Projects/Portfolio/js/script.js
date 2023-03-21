041f70fce273adac2b3d44954bff2e77e744a143
EduardSekan <sekaneduard@gmail.com>
2023-03-21 16:59:15 +0200

    add change

--- Projects/Portfolio/js/script.js
+++ Projects/Portfolio/js/script.js
@@ -1,9 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show")
-}
-
 // Show Hamburger menu
 const hamburger = document.querySelector(".menu-btn")
 const menu = document.querySelector(".menu")
@@ -14,19 +8,6 @@ hamburger.addEventListener("click", function () {
     // body.classList.toggle('overflow-hidden')
 })
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches(".dropbtn")) {
-        let dropdowns = document.getElementsByClassName("dropdown-content")
-        let i
-        for (i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i]
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show")
-            }
-        }
-    }
-}
 // Сhange-theme
 
 function changeTheme() {
