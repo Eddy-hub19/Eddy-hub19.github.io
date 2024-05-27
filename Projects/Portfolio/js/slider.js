048867d6d6d332e8cc2c3d662fec20a682eaeb7b
Eduard <sekaneduard@gmail.com>
2024-05-27 20:25:58 +0300

    add new desing

--- Projects/Portfolio/js/slider.js
+++ Projects/Portfolio/js/slider.js
@@ -1,2 +1,47 @@
 const slider = document.querySelector(".swiper")
 
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 1000,
+
+  // If we need pagination
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+  },
+
+  // autoHeight: true,
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      640: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+      },
+      900: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+      },
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+})
